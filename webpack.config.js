8690e7785fd56c1e38f879488f032f46785a88a7
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-16 23:33:36 -0700

    Add HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,48 @@
-const path = require('path');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  "mode": "none",
-  "entry": "./src/scripts.js",
-  "output": {
-    "path": __dirname + '/dist',
-    "filename": "bundle.js"
+  mode: "none",
+  entry: "./src/scripts.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist')
-    }
+      directory: path.join(__dirname, "dist"),
+    },
   },
-  "devtool": "source-map",
-  "module": {
-    "rules": [
+  devtool: "source-map",
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Overlook Hotel",
+    }),
+  ],
+  module: {
+    rules: [
       {
         test: /\.(css|scss)$/,
         use: [
-            { loader: 'style-loader' },
-            { loader: 'css-loader' },
-            { loader: 'sass-loader' }
-        ]
-    },
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
-              publicPath: 'images/'
-            }
-          }
-        ]
-      }
-    ]
-  }
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
