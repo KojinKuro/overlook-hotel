2a07f75ca6d13ef715025b32dd72ea300d0cdf63
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-18 12:58:01 -0700

    Create data models

---
+++ src/js/data.js
@@ -0,0 +1,17 @@
+let customers = [];
+let rooms = [];
+let bookings = [];
+
+function setCustomers(value) {
+  customers = value;
+}
+
+function setRooms(value) {
+  rooms = value;
+}
+
+function setBookings(value) {
+  bookings = value;
+}
+
+export { bookings, customers, rooms, setBookings, setCustomers, setRooms };
