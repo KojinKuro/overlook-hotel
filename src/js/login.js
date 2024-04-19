75f582421d79612a6a97e8c29f862471af722496
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-18 22:13:42 -0700

    Update login function

--- src/js/login.js
+++ src/js/login.js
@@ -2,7 +2,7 @@ function login(username, password, data) {
   const id = getUsernameID(username);
   if (
     !id ||
-    !data.customers.find((customer) => customer.id === id) ||
+    !data.getCustomers().find((customer) => customer.id === id) ||
     password !== "overlook2021"
   ) {
     return false;
