16609698bafa20dfb65c8ea6772b62fac4c7fd24
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-19 21:47:54 -0700

    remove old function

--- test/bookings-test.js
+++ test/bookings-test.js
@@ -1,7 +1,7 @@
 import chai from "chai";
 import {
   createBooking,
-  filterBookings,
+  // filterBookings,
   isValidBooking,
 } from "../src/js/bookings";
 import { createData } from "../src/js/data";
@@ -88,117 +88,117 @@ describe("Bookings", () => {
     });
   });
 
-  describe("Filter bookings", () => {
-    it("Should filter based on date", () => {
-      const bookings = filterBookings(
-        mockData.getBookings(),
-        new Date(2023, 11, 14)
-      );
-      expect(bookings).to.deep.equal([
-        {
-          id: "5fwrgu4i7k55hl6t9",
-          userID: 21,
-          date: "2023/12/14",
-          roomNumber: 3,
-        },
-      ]);
-    });
+  // describe("Filter bookings", () => {
+  //   it("Should filter based on date", () => {
+  //     const bookings = filterBookings(
+  //       mockData.getBookings(),
+  //       new Date(2023, 11, 14)
+  //     );
+  //     expect(bookings).to.deep.equal([
+  //       {
+  //         id: "5fwrgu4i7k55hl6t9",
+  //         userID: 21,
+  //         date: "2023/12/14",
+  //         roomNumber: 3,
+  //       },
+  //     ]);
+  //   });
 
-    it("Should filter by another date", () => {
-      const bookings = filterBookings(
-        mockData.getBookings(),
-        new Date(2023, 10, 30)
-      );
-      expect(bookings).to.deep.equal([
-        {
-          id: "5fwrgu4i7k55hl6tc",
-          userID: 40,
-          date: "2023/11/30",
-          roomNumber: 4,
-        },
-      ]);
-    });
+  //   it("Should filter by another date", () => {
+  //     const bookings = filterBookings(
+  //       mockData.getBookings(),
+  //       new Date(2023, 10, 30)
+  //     );
+  //     expect(bookings).to.deep.equal([
+  //       {
+  //         id: "5fwrgu4i7k55hl6tc",
+  //         userID: 40,
+  //         date: "2023/11/30",
+  //         roomNumber: 4,
+  //       },
+  //     ]);
+  //   });
 
-    it("Should filter by a range of dates", () => {
-      const bookings = filterBookings(
-        mockData.getBookings(),
-        new Date(2022, 0, 1),
-        new Date(2022, 4, 1)
-      );
+  //   it("Should filter by a range of dates", () => {
+  //     const bookings = filterBookings(
+  //       mockData.getBookings(),
+  //       new Date(2022, 0, 1),
+  //       new Date(2022, 4, 1)
+  //     );
 
-      expect(bookings).to.deep.equal([
-        {
-          date: "2022/04/22",
-          id: "5fwrgu4i7k55hl6sz",
-          roomNumber: 1,
-          userID: 40,
-        },
-        {
-          date: "2022/01/24",
-          id: "5fwrgu4i7k55hl6t5",
-          roomNumber: 1,
-          userID: 21,
-        },
-        {
-          date: "2022/01/10",
-          id: "5fwrgu4i7k55hl6t6",
-          roomNumber: 2,
-          userID: 13,
-        },
-        {
-          date: "2022/02/16",
-          id: "5fwrgu4i7k55hl6t7",
-          roomNumber: 2,
-          userID: 12,
-        },
-        {
-          date: "2022/02/05",
-          id: "5fwrgu4i7k55hl6t8",
-          roomNumber: 3,
-          userID: 40,
-        },
-        {
-          date: "2022/01/11",
-          id: "5fwrgu4i7k55hl6ta",
-          roomNumber: 3,
-          userID: 13,
-        },
-        {
-          date: "2022/02/06",
-          id: "5fwrgu4i7k55hl6tb",
-          roomNumber: 4,
-          userID: 12,
-        },
-        {
-          date: "2022/01/31",
-          id: "5fwrgu4i7k55hl6td",
-          roomNumber: 4,
-          userID: 21,
-        },
-      ]);
-    });
+  //     expect(bookings).to.deep.equal([
+  //       {
+  //         date: "2022/04/22",
+  //         id: "5fwrgu4i7k55hl6sz",
+  //         roomNumber: 1,
+  //         userID: 40,
+  //       },
+  //       {
+  //         date: "2022/01/24",
+  //         id: "5fwrgu4i7k55hl6t5",
+  //         roomNumber: 1,
+  //         userID: 21,
+  //       },
+  //       {
+  //         date: "2022/01/10",
+  //         id: "5fwrgu4i7k55hl6t6",
+  //         roomNumber: 2,
+  //         userID: 13,
+  //       },
+  //       {
+  //         date: "2022/02/16",
+  //         id: "5fwrgu4i7k55hl6t7",
+  //         roomNumber: 2,
+  //         userID: 12,
+  //       },
+  //       {
+  //         date: "2022/02/05",
+  //         id: "5fwrgu4i7k55hl6t8",
+  //         roomNumber: 3,
+  //         userID: 40,
+  //       },
+  //       {
+  //         date: "2022/01/11",
+  //         id: "5fwrgu4i7k55hl6ta",
+  //         roomNumber: 3,
+  //         userID: 13,
+  //       },
+  //       {
+  //         date: "2022/02/06",
+  //         id: "5fwrgu4i7k55hl6tb",
+  //         roomNumber: 4,
+  //         userID: 12,
+  //       },
+  //       {
+  //         date: "2022/01/31",
+  //         id: "5fwrgu4i7k55hl6td",
+  //         roomNumber: 4,
+  //         userID: 21,
+  //       },
+  //     ]);
+  //   });
 
-    it("Should filter by a range of dates", () => {
-      const bookings = filterBookings(
-        mockData.getBookings(),
-        new Date(2023, 0, 1),
-        new Date(2024, 0, 1)
-      );
+  //   it("Should filter by a range of dates", () => {
+  //     const bookings = filterBookings(
+  //       mockData.getBookings(),
+  //       new Date(2023, 0, 1),
+  //       new Date(2024, 0, 1)
+  //     );
 
-      expect(bookings).to.deep.equal([
-        {
-          date: "2023/12/14",
-          id: "5fwrgu4i7k55hl6t9",
-          roomNumber: 3,
-          userID: 21,
-        },
-        {
-          date: "2023/11/30",
-          id: "5fwrgu4i7k55hl6tc",
-          roomNumber: 4,
-          userID: 40,
-        },
-      ]);
-    });
-  });
+  //     expect(bookings).to.deep.equal([
+  //       {
+  //         date: "2023/12/14",
+  //         id: "5fwrgu4i7k55hl6t9",
+  //         roomNumber: 3,
+  //         userID: 21,
+  //       },
+  //       {
+  //         date: "2023/11/30",
+  //         id: "5fwrgu4i7k55hl6tc",
+  //         roomNumber: 4,
+  //         userID: 40,
+  //       },
+  //     ]);
+  //   });
+  // });
 });
