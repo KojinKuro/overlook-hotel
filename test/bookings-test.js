81adf35a5c4c6572d990c581737db71864a33628
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-21 03:19:10 -0700

    Remove filter tests due to using libary code

--- test/bookings-test.js
+++ test/bookings-test.js
@@ -1,7 +1,6 @@
 import chai from "chai";
 import {
   createBooking,
-  filterBookings,
   getCustomerBookings,
   isValidBooking,
   sortBookings,
@@ -101,120 +100,6 @@ describe("Bookings", () => {
         createBooking(40, new Date(2023, 10, 30), 4)
       );
       expect(result).to.deep.equal(false);
-    });
-  });
-
-  describe("Filter bookings", () => {
-    it("Should filter based on date", () => {
-      const bookings = filterBookings(
-        mockData.getBookings(),
-        new Date(2023, 11, 14)
-      );
-      expect(bookings).to.deep.equal([
-        {
-          id: "5fwrgu4i7k55hl6t9",
-          userID: 21,
-          date: "2023/12/14",
-          roomNumber: 3,
-        },
-      ]);
-    });
-
-    it("Should filter by another date", () => {
-      const bookings = filterBookings(
-        mockData.getBookings(),
-        new Date(2023, 10, 30)
-      );
-      expect(bookings).to.deep.equal([
-        {
-          id: "5fwrgu4i7k55hl6tc",
-          userID: 40,
-          date: "2023/11/30",
-          roomNumber: 4,
-        },
-      ]);
-    });
-
-    it("Should filter by a range of dates", () => {
-      const bookings = filterBookings(
-        mockData.getBookings(),
-        new Date(2022, 0, 1),
-        new Date(2022, 4, 1)
-      );
-
-      expect(bookings).to.deep.equal([
-        {
-          id: "5fwrgu4i7k55hl6sz",
-          userID: 40,
-          date: "2022/04/22",
-          roomNumber: 1,
-        },
-        {
-          id: "5fwrgu4i7k55hl6t5",
-          userID: 21,
-          date: "2022/01/24",
-          roomNumber: 1,
-        },
-        {
-          id: "5fwrgu4i7k55hl6t6",
-          userID: 13,
-          date: "2022/01/10",
-          roomNumber: 2,
-        },
-        {
-          id: "5fwrgu4i7k55hl6t7",
-          userID: 12,
-          date: "2022/02/16",
-          roomNumber: 2,
-        },
-        {
-          id: "5fwrgu4i7k55hl6t8",
-          userID: 40,
-          date: "2022/02/05",
-          roomNumber: 3,
-        },
-        {
-          id: "5fwrgu4i7k55hl6ta",
-          userID: 13,
-          date: "2022/01/11",
-          roomNumber: 3,
-        },
-        {
-          id: "5fwrgu4i7k55hl6tb",
-          userID: 12,
-          date: "2022/02/06",
-          roomNumber: 4,
-        },
-        {
-          id: "5fwrgu4i7k55hl6td",
-          userID: 21,
-          date: "2022/01/31",
-          roomNumber: 4,
-        },
-      ]);
-    });
-
-    it("Should filter by a range of dates", () => {
-      const bookings = filterBookings(
-        mockData.getBookings(),
-        new Date(2023, 0, 1),
-        new Date(2024, 0, 1)
-      );
-
-      expect(bookings).to.deep.equal([
-        {
-          date: "2023/12/14",
-          id: "5fwrgu4i7k55hl6t9",
-          roomNumber: 3,
-          userID: 21,
-        },
-        {
-          date: "2023/11/30",
-          id: "5fwrgu4i7k55hl6tc",
-          roomNumber: 4,
-          userID: 40,
-        },
-      ]);
     });
   });
 
