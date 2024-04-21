b2b83bd6510cf560dc99dac56e4c70a923c461f3
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-20 23:22:55 -0700

    feature: filter booking on all, past, future

--- src/dom/domHistory.js
+++ src/dom/domHistory.js
@@ -1,3 +1,4 @@
+import { setDOM } from "../domUpdates";
 import {
   filterBookings,
   getCustomerBookings,
@@ -14,7 +15,19 @@ import {
 
 let bookingFilter = "all";
 
-document.getElementById("root").addEventListener("click", () => {});
+document.getElementById("root").addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.closest(".booking-nav-container")) {
+    if (e.target.classList.contains("all-booking-button")) {
+      bookingFilter = "all";
+    } else if (e.target.classList.contains("past-booking-button")) {
+      bookingFilter = "past";
+    } else if (e.target.classList.contains("future-booking-button")) {
+      bookingFilter = "future";
+    }
+    setDOM(e.currentTarget, historyPage);
+  }
+});
 
 export function historyPage() {
   return `
@@ -25,7 +38,7 @@ export function historyPage() {
   </nav>
   <hr>
   <div>
-    <nav>
+    <nav class="booking-nav-container">
       <button class="all-booking-button">All Bookings</button>
       <button class="past-booking-button">Past Bookings</button>
       <button class="future-booking-button">Future Bookings</button>
@@ -40,23 +53,28 @@ export function historyPage() {
 
 function usersBookingHTML(id, filter, data) {
   let bookings = getCustomerBookings(id, data);
-  let min, max;
+  let min, max, sortAscending;
   switch (filter) {
     case "all":
       min = pastDate;
       max = futureDate;
+      sortAscending = false;
       break;
     case "past":
       min = pastDate;
       max = todayDate;
+      sortAscending = false;
       break;
     case "future":
       min = todayDate;
       max = futureDate;
+      sortAscending = true;
       break;
   }
 
-  bookings = sortBookings(filterBookings(bookings, min, max));
+  bookings = filterBookings(bookings, min, max);
+  bookings = sortBookings(bookings, sortAscending);
+
   return bookings.reduce((html, booking) => {
     html += bookingHistoryCardHTML(booking, data.getRooms());
     html += "<br>";
