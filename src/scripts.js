7eeebf0e4d8273139088fdcc9cf2f7bece5be3d1
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-04-21 05:14:02 -0700

    Generate roomSettings

--- src/scripts.js
+++ src/scripts.js
@@ -7,16 +7,19 @@ import "./css/styles.scss";
 import "./domUpdates";
 import "./images/turing-logo.png";
 
-import { historyPage } from "./dom/domHistory";
+import { bookingPage } from "./dom/domBooking";
+// import { historyPage } from "./dom/domHistory";
 // import { loginPage } from "./dom/domLogin";
 import { setDOM } from "./domUpdates";
 import { getAllData } from "./js/apiCalls";
 import { getCustomer } from "./js/customers";
 import { createData } from "./js/data";
+import { generateRoomOptions } from "./js/rooms";
 
 // global data
 export const localData = createData();
 export let currentCustomer = null;
+export let roomSettings = null;
 // used for debugging what currentCustomer is
 // run `user()` in console to run
 global.user = () => currentCustomer;
@@ -29,11 +32,16 @@ function setCustomer(value) {
 
 function init() {
   getAllData(localData).then(() => {
+    roomSettings = generateRoomOptions(localData.getRooms(), [
+      "numBeds",
+      "roomType",
+      "bedSize",
+    ]);
+
     // auto set currentCustomer
     setCustomer(getCustomer(localData.getCustomers(), 20));
-
     // set page current to history for debugging
-    setDOM(document.querySelector("#root"), historyPage);
+    setDOM(document.querySelector("#root"), bookingPage);
     // setDOM(document.querySelector("#root"), loginPage);
   });
 }
